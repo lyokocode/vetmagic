035c4599678015f0c577bb22eca32da34c07c7d1
lyokocode <aelita.dev@gmail.com>
2024-02-14 02:45:45 +0300

    single blog UI 1.0.0

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import Image from "next/image"
+import styles from "./singleBlog.module.scss"
 
 const SingleBlog = () => {
     return (
-        <div>SingleBlog</div>
+        <div className={styles.singleBlog}>
+            <div className={styles.imageContainer}>
+                <Image fill sizes='100%' src="/card1.png" alt='blog image' />
+            </div>
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>Cerrahi Müdahaleden Önce hasta sahiplerinin yapması gerekenler </h1>
+                <div className={styles.detail}>
+                    <Image
+                        src="/card2.png"
+                        alt='blog image'
+                        className={styles.avatar}
+                        width={50}
+                        height={50}
+                    />
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Author</span>
+                        <span className={styles.detailValue}>İsmail Altıntop</span>
+                    </div>
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Published</span>
+                        <span className={styles.detailValue}>01.01.2024</span>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit consectetur magni error quasi mollitia officiis voluptas fugiat recusandae, nam sapiente, sint, cumque voluptatibus minus nulla? Iure consequatur harum ducimus animi tenetur, dignissimos vel magni adipisci nulla labore recusandae quasi numquam, neque sapiente eligendi, repellendus architecto nemo enim eaque. Ab, reiciendis reprehenderit est autem perferendis magnam et porro animi esse ratione? Veniam corrupti maxime totam illum, fugiat ipsa autem provident magnam.
+                </div>
+            </div>
+        </div>
     )
 }
 
